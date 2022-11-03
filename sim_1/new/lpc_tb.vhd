afd46d0e0b1a6139d775ade73d1ff68bbd387832
Nicholas Chin <nic.c3.14@gmail.com>
2022-11-02 21:48:53 -0600

    Testbenches: Clean up code

--- sim_1/new/lpc_tb.vhd
+++ sim_1/new/lpc_tb.vhd
@@ -1,38 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/15/2021 11:07:48 AM
--- Design Name: 
--- Module Name: lpc_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity lpc_tb is
---  Port ( );
 end lpc_tb;
 
 architecture Behavioral of lpc_tb is
